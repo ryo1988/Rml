6b07c08662326f546be4059ba8d8ad8a2de805b8
ryo1988 <ryoichi1988@gmail.com>
2017-10-11 18:52:30 +0900

    DescriptionExtension.cs 追加

---
+++ Rml/DescriptionExtension.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace Rml
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class DescriptionExtension
+    {
+        /// <summary>
+        /// <see cref="DescriptionAttribute.Description"/>を取得します
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TProperty"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="propertyExpression"></param>
+        /// <returns></returns>
+        public static string GetDescription<TSource, TProperty>(this TSource source,
+            Expression<Func<TSource, TProperty>> propertyExpression)
+        {
+            if (propertyExpression == null)
+                throw new ArgumentNullException(nameof(propertyExpression));
+
+            if (propertyExpression.Body is MemberExpression == false)
+                throw new ArgumentException(nameof(propertyExpression));
+
+            var memberInfo = ((MemberExpression)propertyExpression.Body).Member;
+            if (memberInfo == null)
+            {
+                return null;
+            }
+
+            var attributes = (DescriptionAttribute[])memberInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            return attributes.Length > 0 && !String.IsNullOrEmpty(attributes[0].Description) ? attributes[0].Description : memberInfo.Name;
+        }
+    }
+}
