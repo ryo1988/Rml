76929a02fcd19c9693679684fcb2ef3fd67bfb7d
ryo1988 <ryoichi1988@gmail.com>
2017-10-25 17:57:25 +0900

    BindingProxy.cs 追加

---
+++ Rml.Wpf/BindingProxy.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace Rml.Wpf
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class BindingProxy : Freezable
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly DependencyProperty DataProperty = DependencyProperty.Register("Data", typeof(object), typeof(BindingProxy), new UIPropertyMetadata(null));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public object Data
+        {
+            get => GetValue(DataProperty);
+            set => SetValue(DataProperty, value);
+        }
+
+        /// <inheritdoc />
+        protected override Freezable CreateInstanceCore()
+        {
+            return new BindingProxy();
+        }
+    }
+}
