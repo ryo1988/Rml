c9378b98ac4ec93c8526e909cdd614d751f3b9b4
ryo1988 <ryoichi1988@gmail.com>
2018-05-13 01:49:34 +0900

    assemblyを特定のフォルダを優先して読み込むライブラリ追加

---
+++ Rml.RedirectLoadAssemblyFolder/RedirectLoadAssemblyFolder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Rml.RedirectLoadAssemblyFolder
+{
+    public class RedirectLoadAssemblyFolder
+    {
+        public void Redirect(string folderPath)
+        {
+            AttachHandler(folderPath);
+        }
+
+        public void RedirectExecutingAssemblyFolder()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var folderPath = Path.GetDirectoryName(assembly.Location);
+            AttachHandler(folderPath);
+        }
+
+        private static void AttachHandler(string folderPath)
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += (s, e) =>
+            {
+                var info = e.Name.Split(',');
+                var path = Path.Combine(folderPath, info[0] + ".dll");
+
+                try
+                {
+                    var assembly = Assembly.LoadFile(path);
+                    return assembly;
+                }
+                catch
+                {
+                    // ignored
+                }
+
+                return null;
+            };
+        }
+    }
+}
