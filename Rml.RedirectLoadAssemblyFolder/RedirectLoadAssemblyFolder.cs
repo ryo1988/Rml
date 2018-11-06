f4b8fb3221211571193234682ea1b28ad986205c
ryo1988 <ryoichi1988@gmail.com>
2018-11-06 13:59:01 +0900

    リダイレクト時に、既に読み込みアセンブリがある場合はそれを使用するように

--- Rml.RedirectLoadAssemblyFolder/RedirectLoadAssemblyFolder.cs
+++ Rml.RedirectLoadAssemblyFolder/RedirectLoadAssemblyFolder.cs
@@ -29,38 +29,49 @@ namespace Rml.RedirectLoadAssemblyFolder
             RedirectExecutingAssemblyFolder(AppDomain.CurrentDomain);
         }
 
+        private static Assembly LoadAssembly(HashSet<string> haveAssembly, string folderPath, AppDomain appDomain, string location, string name)
+        {
+            var requestingAssemblyFolderPath = Path.GetDirectoryName(location);
+            if (folderPath != requestingAssemblyFolderPath && folderPath != appDomain.BaseDirectory)
+            {
+                return null;
+            }
+
+            var assemblyName = new AssemblyName(name);
+            var path = Path.Combine(folderPath, assemblyName.Name + ".dll");
+            if (haveAssembly.Contains(path) == false)
+            {
+                return null;
+            }
+
+            try
+            {
+                foreach(var assembly in appDomain.GetAssemblies())
+                {
+                    if (assembly.FullName == name)
+                    {
+                        return assembly;
+                    }
+                }
+                {
+                    var assembly = Assembly.LoadFile(path);
+                    return assembly;
+                }
+            }
+            catch
+            {
+                // ignored
+            }
+
+            return null;
+        }
+
         private static void AttachHandler(string folderPath, AppDomain appDomain)
         {
             var files = Directory.GetFiles(folderPath, "*.dll", SearchOption.AllDirectories);
             var haveAssembly = new HashSet<string>(files);
 
-            appDomain.AssemblyResolve += (s, e) =>
-            {
-                var requestingAssemblyFolderPath = Path.GetDirectoryName(e.RequestingAssembly.Location);
-                if (folderPath != requestingAssemblyFolderPath)
-                {
-                    return null;
-                }
-
-                var assemblyName = new AssemblyName(e.Name);
-                var path = Path.Combine(folderPath, assemblyName.Name + ".dll");
-                if (haveAssembly.Contains(path) == false)
-                {
-                    return null;
-                }
-
-                try
-                {
-                    var assembly = Assembly.LoadFile(path);
-                    return assembly;
-                }
-                catch
-                {
-                    // ignored
-                }
-
-                return null;
-            };
+            appDomain.AssemblyResolve += (s, e) => LoadAssembly(haveAssembly, folderPath, appDomain, e.RequestingAssembly?.Location ?? Assembly.GetCallingAssembly().Location, e.Name);
         }
     }
 }
