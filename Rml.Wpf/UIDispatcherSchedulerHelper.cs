7be6c4c21b15652cceeb9650b0936eb38451ea98
ryo1988 <ryoichi1988@gmail.com>
2018-11-08 00:41:07 +0900

    UIDispatcherSchedulerHelper追加

---
+++ Rml.Wpf/UIDispatcherSchedulerHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using System.Threading;
+using System.Windows.Threading;
+using Reactive.Bindings;
+
+namespace Rml.Wpf
+{
+    public static class UIDispatcherSchedulerHelper
+    {
+        public static void ScheduleUIDispatcher(Action action)
+        {
+            try
+            {
+                UIDispatcherScheduler.Default.Schedule(action);
+            }
+            catch
+            {
+                var thread = new Thread(() =>
+                {
+                    var dispatcherSynchronizationContext = new DispatcherSynchronizationContext(Dispatcher.CurrentDispatcher);
+                    SynchronizationContext.SetSynchronizationContext(dispatcherSynchronizationContext);
+
+                    UIDispatcherScheduler.Default.Schedule(action);
+
+                    Dispatcher.Run();
+                });
+                thread.SetApartmentState(ApartmentState.STA);
+                thread.Start();
+            }
+        }
+    }
+}
