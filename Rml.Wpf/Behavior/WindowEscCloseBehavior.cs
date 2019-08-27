3ffc45300b57fc762bb34f4e04f88e8f125f3b7f
Kazunori Mitani <kn.mitani@digital-support.co.jp>
2019-08-27 12:08:53 +0900

    EscキーでWindowを閉じるBehaviorを追加

---
+++ Rml.Wpf/Behavior/WindowEscCloseBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace Rml.Wpf.Behavior
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class WindowEscCloseBehavior : Behavior<FrameworkElement>
+    {
+        /// <inheritdoc />
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.KeyDown += AssociatedObject_KeyDown;
+        }
+
+        /// <inheritdoc />
+        protected override void OnDetaching()
+        {
+            AssociatedObject.KeyDown -= AssociatedObject_KeyDown;
+            base.OnDetaching();
+        }
+
+        void AssociatedObject_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                var window = AssociatedObject as Window ?? Window.GetWindow(AssociatedObject);
+                if (window == null)
+                {
+                    return;
+                }
+                window.Close();
+            }
+        }
+    }
+}
