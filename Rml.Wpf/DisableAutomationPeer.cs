48d1ddc37ec8ef26a23858e3f020248295d508b4
ryo1988 <ryoichi1988@gmail.com>
2020-04-01 13:49:57 +0900

    DisableAutomationPeer追加

---
+++ Rml.Wpf/DisableAutomationPeer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Automation.Peers;
+
+namespace Rml.Wpf
+{
+    /// <summary>
+    /// UIオートメーションを無効化
+    /// </summary>
+    public class DisableAutomationPeer : FrameworkElementAutomationPeer
+    {
+        private static readonly List<AutomationPeer> Children = new List<AutomationPeer>();
+
+        /// <inheritdoc />
+        public DisableAutomationPeer(FrameworkElement owner) : base(owner) { }
+
+        /// <inheritdoc />
+        protected override string GetNameCore()
+        {
+            return nameof(DisableAutomationPeer);
+        }
+
+        /// <inheritdoc />
+        protected override AutomationControlType GetAutomationControlTypeCore()
+        {
+            return AutomationControlType.Window;
+        }
+
+        /// <inheritdoc />
+        protected override List<AutomationPeer> GetChildrenCore()
+        {
+            return Children;
+        }
+    }
+}
