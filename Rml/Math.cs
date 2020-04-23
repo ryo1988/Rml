5718f297438deb2ce741df975d78a5291953d9a4
ryo1988 <ryoichi1988@gmail.com>
2020-04-23 11:28:59 +0900

    Math.Round追加

---
+++ Rml/Math.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Rml
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class Math
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="decimals"></param>
+        /// <param name="mode"></param>
+        /// <returns></returns>
+        public static double Round(double value, int decimals, MidpointRounding mode)
+        {
+            var pow = System.Math.Pow(10, decimals);
+            return System.Math.Round(value * pow, mode) / pow;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="decimals"></param>
+        /// <param name="mode"></param>
+        /// <returns></returns>
+        public static int Round(int value, int decimals, MidpointRounding mode)
+        {
+            var pow = System.Math.Pow(10, decimals);
+            return (int)(System.Math.Round(value * pow, mode) / pow);
+        }
+    }
+}
