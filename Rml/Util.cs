32027d76b2cd0c0dc9523d31eb542bcbc3c4f531
ryo1988 <ryoichi1988@gmail.com>
2020-08-04 12:56:22 +0900

    .NetCore向けにBasePathを取る関数を追加

---
+++ Rml/Util.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Rml
+{
+    public static class Util
+    {
+        public static string GetBasePath()
+        {
+            var mainModuleFileName = Process.GetCurrentProcess().MainModule?.FileName;
+            if (mainModuleFileName == @"c:\program files\dotnet\dotnet.exe")
+            {
+                return Directory.GetCurrentDirectory();
+            }
+
+            return Path.GetDirectoryName(Process.GetCurrentProcess().MainModule?.FileName ??
+                                         throw new InvalidOperationException()) ??
+                   throw new InvalidOperationException();
+        }
+    }
+}
