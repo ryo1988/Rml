cd1952cfa03ba162c9a2a74b0f37aa8f54a1037b
ryo1988 <ryoichi1988@gmail.com>
2020-08-12 15:20:46 +0900

    BindingProxy拡張

--- Rml.Wpf/BindingProxy.cs
+++ Rml.Wpf/BindingProxy.cs
@@ -27,4 +27,31 @@ namespace Rml.Wpf
             return new BindingProxy();
         }
     }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class BindingProxy<T> : Freezable
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly DependencyProperty DataProperty = DependencyProperty.Register("Data", typeof(T), typeof(BindingProxy<T>), new UIPropertyMetadata(null));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public T Data
+        {
+            get => (T)GetValue(DataProperty);
+            set => SetValue(DataProperty, value);
+        }
+
+        /// <inheritdoc />
+        protected override Freezable CreateInstanceCore()
+        {
+            return new BindingProxy();
+        }
+    }
 }
