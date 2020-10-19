8025484ba30c99a2dae780faf9e87ad5447e5e2d
ryo1988 <ryoichi1988@gmail.com>
2020-10-19 11:44:34 +0900

    SequenceEqualEqualityComparer追加

---
+++ Rml/SequenceEqualEqualityComparer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Rml
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class SequenceEqualEqualityComparer<T> : IEqualityComparer<T[]>
+    {
+        /// <inheritdoc />
+        public bool Equals(T[]? x, T[]? y)
+        {
+            return (x ?? Array.Empty<T>()).SequenceEqual(y ?? Array.Empty<T>());
+        }
+
+        /// <inheritdoc />
+        public int GetHashCode(T[]? obj)
+        {
+            return obj?.GetHashCode() ?? 0;
+        }
+    }
+}
