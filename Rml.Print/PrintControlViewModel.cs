a98feaa66f6abd093b62c53d0260cf30c16ec70a
ryo1988 <ryoichi1988@gmail.com>
2020-11-02 12:12:52 +0900

    Rml.Print追加

---
+++ Rml.Print/PrintControlViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Printing;
+using System.Windows;
+using System.Windows.Media;
+using SUT.PrintEngine.Extensions;
+using SUT.PrintEngine.Paginators;
+using SUT.PrintEngine.Views;
+
+namespace Rml.Print
+{
+    public class PrintControlViewModel : SUT.PrintEngine.ViewModels.PrintControlViewModel
+    {
+        public PrintControlViewModel(PrintControlView view)
+            : base(view)
+        {
+        }
+
+        public PrintControlViewModel(Visual visual, Rect viewBox, Rect viewport)
+            : this(new PrintControlView())
+        {
+            var drawingVisual = new DrawingVisual();
+            using (var drawingContext = drawingVisual.RenderOpen())
+            {
+                var visualBrush = new VisualBrush(visual)
+                {
+                    Stretch = Stretch.None,
+                    Viewbox = viewBox,
+                };
+                drawingContext.DrawRectangle(visualBrush, null, viewport);
+                drawingContext.PushOpacityMask(Brushes.White);
+            }
+
+            DrawingVisual = drawingVisual;
+        }
+
+        public override void ExecutePrint(object parameter)
+        {
+            try
+            {
+                // 何故かうまくプリントできないので直接プリント
+                ShowProgressDialog();
+                ((VisualPaginator) Paginator).PageCreated += PrintControlPresenterPageCreated;
+                var writer = PrintQueue.CreateXpsDocumentWriter(CurrentPrinter);
+                writer.Write(Paginator, CurrentPrinter.UserPrintTicket);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Hand);
+            }
+            finally
+            {
+                ProgressDialog.Hide();
+            }
+        }
+
+        private void PrintControlPresenterPageCreated(object sender, PageEventArgs e)
+        {
+            ProgressDialog.CurrentProgressValue = e.PageNumber;
+            ProgressDialog.Message = GetStatusMessage();
+            Application.Current.DoEvents();
+        }
+    }
+}
