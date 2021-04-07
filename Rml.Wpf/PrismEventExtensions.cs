ea99361cf11b7b2cf96d14b8dc5aadff2eefd9d2
ryo1988 <ryoichi1988@gmail.com>
2021-04-07 17:38:00 +0900

    PrismEventExtensions追加

---
+++ Rml.Wpf/PrismEventExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reactive.Linq;
+using Prism.Events;
+
+namespace Rml.Wpf
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public static class PrismEventExtensions
+    {
+        /// <summary>
+        /// PubSubEvent to IObservable
+        /// </summary>
+        /// <param name="self"></param>
+        /// <typeparam name="TPayload"></typeparam>
+        /// <returns></returns>
+        public static IObservable<TPayload> ToObservable<TPayload>(this PubSubEvent<TPayload> self)
+        {
+            return Observable.Create<TPayload>(observer => self.Subscribe(observer.OnNext));
+        }
+    }
+}
