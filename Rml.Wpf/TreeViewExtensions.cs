cc1e032c60771bb7dc4ff4beaafd8590b50b87ef
ryo1988 <ryoichi1988@gmail.com>
2021-04-19 18:05:38 +0900

    TreeViewExtensions追加

---
+++ Rml.Wpf/TreeViewExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows.Controls;
+
+namespace Rml.Wpf
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public static class TreeViewExtensions
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="container"></param>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public static TreeViewItem GetTreeViewItem(this ItemsControl container, object item)
+        {
+            if (container == null) return null;
+
+            if (container.DataContext == item)
+            {
+                return container as TreeViewItem;
+            }
+
+            if (container is TreeViewItem treeViewItem && !treeViewItem.IsExpanded)
+            {
+                treeViewItem.SetValue(TreeViewItem.IsExpandedProperty, true);
+            }
+
+            container.ApplyTemplate();
+
+            for (int i = 0, count = container.Items.Count; i < count; i++)
+            {
+                var subContainer = (TreeViewItem) container.ItemContainerGenerator.ContainerFromIndex(i);
+
+                subContainer.BringIntoView();
+
+                var resultContainer = GetTreeViewItem(subContainer, item);
+                if (resultContainer != null)
+                {
+                    return resultContainer;
+                }
+
+                subContainer.IsExpanded = false;
+            }
+
+            return null;
+        }
+    }
+}
