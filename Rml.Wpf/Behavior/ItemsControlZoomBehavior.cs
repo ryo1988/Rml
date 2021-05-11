685a197520f44a260733d0d85d451c95ec1c0b23
ryo1988 <ryoichi1988@gmail.com>
2021-05-11 11:04:47 +0900

    ItemsControlZoomBehavior追加

---
+++ Rml.Wpf/Behavior/ItemsControlZoomBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using Microsoft.Xaml.Behaviors;
+
+namespace Rml.Wpf.Behavior
+{
+    /// <summary>
+    /// Ctrl+ホイールでFontSizeを調整します
+    /// </summary>
+    public class ItemsControlZoomBehavior : Behavior<ItemsControl>
+    {
+        /// <inheritdoc />
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.PreviewMouseWheel += AssociatedObjectOnPreviewMouseWheel;
+        }
+
+        /// <inheritdoc />
+        protected override void OnDetaching()
+        {
+            AssociatedObject.PreviewMouseWheel -= AssociatedObjectOnPreviewMouseWheel;
+
+            base.OnDetaching();
+        }
+
+        private void AssociatedObjectOnPreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (Keyboard.Modifiers != ModifierKeys.Control)
+                return;
+
+            var fontSize = System.Math.Max(System.Math.Floor(AssociatedObject.FontSize + (double)e.Delta / 120), 1);
+            AssociatedObject.SetCurrentValue(Control.FontSizeProperty, fontSize);
+
+            e.Handled = true;
+        }
+    }
+}
