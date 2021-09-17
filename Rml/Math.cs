09a6639168182e7b19295b9e716bcea0d3efad67
ryo1988 <ryoichi1988@gmail.com>
2021-09-17 11:26:52 +0900

    RoundInt追加

--- Rml/Math.cs
+++ Rml/Math.cs
@@ -14,7 +14,7 @@ namespace Rml
         /// <param name="digits"></param>
         /// <param name="mode"></param>
         /// <returns></returns>
-        public static float Round(float value, int digits, MidpointRounding mode)
+        public static float Round(this float value, int digits, MidpointRounding mode)
         {
             return (float) System.Math.Round(value, digits, mode);
         }
@@ -25,7 +25,7 @@ namespace Rml
         /// <param name="digits"></param>
         /// <param name="mode"></param>
         /// <returns></returns>
-        public static double Round(double value, int digits, MidpointRounding mode)
+        public static double Round(this double value, int digits, MidpointRounding mode)
         {
             return System.Math.Round(value, digits, mode);
         }
@@ -37,10 +37,21 @@ namespace Rml
         /// <param name="digits"></param>
         /// <param name="mode"></param>
         /// <returns></returns>
-        public static int Round(int value, int digits, MidpointRounding mode)
+        public static int Round(this int value, int digits, MidpointRounding mode)
         {
             var pow = System.Math.Pow(10.0, digits);
             return (int) (System.Math.Round(value / pow, digits, mode) * pow);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="mode"></param>
+        /// <returns></returns>
+        public static int RoundInt(this double value, MidpointRounding mode = MidpointRounding.AwayFromZero)
+        {
+            return (int)Round(value, 0, mode);
         }
     }
 }
