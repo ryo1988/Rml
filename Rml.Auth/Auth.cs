9c3277b5cc479f837b02a3cf36155cdb00bdc0e7
ryo1988 <ryoichi1988@gmail.com>
2021-11-09 17:10:18 +0900

    認証関係追加

---
+++ Rml.Auth/Auth.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace Rml.Auth
+{
+    public static class Auth
+    {
+        public static async Task<string> GetTokenMsalAsync(string url, string clientId, string tenantId, CancellationToken cancellationToken)
+        {
+            var uri = new Uri(url);
+
+            var publicClientApp = PublicClientApplicationBuilder
+                .Create(clientId)
+                .WithDefaultRedirectUri()
+                .WithAuthority(AzureCloudInstance.AzurePublic, tenantId)
+                .Build();
+
+            var authResult = await publicClientApp
+                .AcquireTokenInteractive(new[]
+                {
+                    $"{uri.Scheme}://{uri.Authority}//AllSites.Manage"
+                })
+                .WithUseEmbeddedWebView(false)
+                .ExecuteAsync(cancellationToken);
+
+            return authResult.AccessToken;
+        }
+    }
+}
---
+++ Rml.Sharepoint/Sharepoint.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+
+namespace Rml.Sharepoint
+{
+    public static class Sharepoint
+    {
+        public static async Task<ClientContext> GetClientContextAsync(string url, string token)
+        {
+            var clientContext = new ClientContext(url);
+            clientContext.ExecutingWebRequest += (_, args) =>
+                args.WebRequestExecutor.RequestHeaders["Authorization"] = "Bearer " + token;
+
+            try
+            {
+                clientContext.Load(clientContext.Web);
+                await clientContext.ExecuteQueryAsync();
+            }
+            catch
+            {
+                return null;
+            }
+
+            return clientContext;
+        }
+    }
+}
