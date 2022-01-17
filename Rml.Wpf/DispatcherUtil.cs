2e68dabafc7d7032f8c961790c4d292bb6a031ac
ryo1988 <ryoichi1988@gmail.com>
2022-01-17 12:57:22 +0900

    Dispatcherを動かしつつ待つ処理を追加

---
+++ Rml.Wpf/DispatcherUtil.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Rml.Wpf;
+
+public static class DispatcherUtil
+{
+    public static T WaitAndPushDispatcher<T>(this Task<T> task, DispatcherPriority priority = DispatcherPriority.Background)
+    {
+        WaitAndPushDispatcher(task as Task);
+
+        return task.Result;
+    }
+
+    public static void WaitAndPushDispatcher(this Task task, DispatcherPriority priority = DispatcherPriority.Background)
+    {
+        var spinWait = new SpinWait();
+
+        var complited = false;
+        task.ContinueWith(_ => complited = true);
+        while (complited is false)
+        {
+            Dispatcher.CurrentDispatcher.Invoke(() => { }, priority);
+            spinWait.SpinOnce();
+        }
+    }
+}
