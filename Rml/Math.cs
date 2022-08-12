bdf80676f08b60e9fba7ad3cedcff11bc7e4e04c
ryo1988 <ryoichi1988@gmail.com>
2022-08-12 11:31:05 +0900

    36進数の変換処理追加

--- Rml/Math.cs
+++ Rml/Math.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
 
 namespace Rml
 {
@@ -63,6 +65,49 @@ namespace Rml
         public static int RoundInt(this double value, MidpointRounding mode = MidpointRounding.AwayFromZero)
         {
             return (int)Round(value, 0, mode);
+        }
+
+        private static readonly char[] Base36String = new[]
+        {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z'
+        };
+
+        private static readonly ImmutableDictionary<char, int> Base36ToInt = Base36String
+            .Select((o, i) => (value: o, index: i))
+            .ToImmutableDictionary(o => o.value, o => o.index);
+
+        private static readonly ImmutableDictionary<int, char> IntToBase36 = Base36String
+            .Select((o, i) => (value: o, index: i))
+            .ToImmutableDictionary(o => o.index, o => o.value);
+
+        public static int ConvertBase36ToInt(this ReadOnlySpan<char> base36)
+        {
+            int value = 0;
+            
+            foreach (var c in base36.Trim())
+            {
+                value = value * Base36String.Length + Base36ToInt[c];
+            }
+
+            return value;
+        }
+
+        public static string ConvertIntToBase36(this int intValue)
+        {
+            var length = (int)System.Math.Log(intValue, Base36String.Length) + 1;
+            
+            Span<char> base36 = stackalloc char[length];
+
+            for (int i = length - 1; i >= 0; i--)
+            {
+                base36[i] = IntToBase36[intValue % Base36String.Length];
+                intValue /= Base36String.Length;
+            }
+
+            return base36.ToString();
         }
     }
 }
