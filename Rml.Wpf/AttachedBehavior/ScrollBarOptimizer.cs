4b1298ce3d073c80dedb7ebb1d453867d54a4ac4
ryo1988 <ryoichi1988@gmail.com>
2023-05-18 11:27:21 +0900

    ScrollBarOptimizer追加

---
+++ Rml.Wpf/AttachedBehavior/ScrollBarOptimizer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Media;
+using Rml.Wpf.Command;
+
+namespace Rml.Wpf.AttachedBehavior;
+
+public static class ScrollBarOptimizer
+{
+    public static readonly DependencyProperty EnableOptimizeCommandProperty = DependencyProperty.RegisterAttached(
+        "EnableOptimizeCommand", typeof(bool), typeof(ScrollBarOptimizer),
+        new PropertyMetadata(default(bool), EnableOptimizeCommandChanged));
+
+    public static void SetEnableOptimizeCommand(DependencyObject element, bool value)
+        => element.SetValue(EnableOptimizeCommandProperty, value);
+
+    public static bool GetEnableOptimizeCommand(DependencyObject element)
+        => (bool)element.GetValue(EnableOptimizeCommandProperty);
+
+    private static void EnableOptimizeCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (e.NewValue is false)
+            return;
+        
+        if (d is not ScrollBar bar)
+            throw new NotSupportedException("ScrollBarのみ対応");
+
+        if (bar.IsLoaded)
+            throw new InvalidOperationException("");
+
+        // ScrollBarのボタンはControlTemplateに定義されているので、Loaded後に作成される
+        bar.Loaded += ScrollBarLoaded;
+    }
+
+    private static void ScrollBarLoaded(object sender, RoutedEventArgs e)
+    {
+        var bar = (ScrollBar)sender;
+        
+        bar.Loaded -= ScrollBarLoaded;
+
+        // ボタンがない場合もあるので、ApplyTemplateしておく
+        bar.ApplyTemplate();
+
+        foreach (var button in GetVisualDescendants(bar).OfType<ButtonBase>())
+            // スクロール対象がない場合はスクロールバー自体無効化されるので、使っているボタンは常に実行可能にしてRoutedEventsの発生を抑制
+            button.Command = new AlwaysExecutableRoutedCommand((RoutedCommand)button.Command, button);
+    }
+
+    private static IEnumerable<DependencyObject> GetVisualDescendants(DependencyObject target)
+    {
+        for (var i = 0; i < VisualTreeHelper.GetChildrenCount(target); i++)
+        {
+            var child = VisualTreeHelper.GetChild(target, i);
+
+            yield return child;
+
+            foreach (var descendant in GetVisualDescendants(child)) yield return descendant;
+        }
+    }
+}
---
+++ Rml.Wpf/Command/AlwaysExecutableRoutedCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Rml.Wpf.Command;
+
+public sealed class AlwaysExecutableRoutedCommand : ICommand
+{
+    private readonly RoutedCommand _originalCommand;
+    private readonly IInputElement _target;
+
+    public AlwaysExecutableRoutedCommand(RoutedCommand originalCommand, IInputElement target)
+    {
+        _originalCommand = originalCommand ?? throw new ArgumentNullException(nameof(originalCommand));
+        _target = target ?? throw new ArgumentNullException(nameof(target));
+    }
+
+    public event EventHandler CanExecuteChanged
+    {
+        add { }
+        remove { }
+    }
+
+    public bool CanExecute(object parameter) => true;
+
+    public void Execute(object parameter) => _originalCommand.Execute(parameter, _target);
+}
