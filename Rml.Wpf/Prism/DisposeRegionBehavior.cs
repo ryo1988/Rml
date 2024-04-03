4d23712afd54a5766da4831b3d617aad9f39374c
ryo1988 <6487424+ryo1988@users.noreply.github.com>
2024-04-03 14:26:16 +0900

    DisposeRegionBehaviorを追加

---
+++ Rml.Wpf/Prism/DisposeRegionBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Specialized;
+using Prism.Common;
+using Prism.Regions;
+
+namespace Rml.Wpf.Prism;
+
+public class DisposeRegionBehavior : IRegionBehavior
+{
+    public const string Key = nameof(DisposeRegionBehavior);
+    public IRegion Region { get; set; }
+
+    public void Attach()
+    {
+        Region.Views.CollectionChanged += Views_CollectionChanged;
+    }
+
+    private void Views_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (e.Action is NotifyCollectionChangedAction.Remove)
+        {
+            if (e.OldItems is null)
+                return;
+            
+            foreach (var o in e.OldItems)
+            {
+                MvvmHelpers.ViewAndViewModelAction<IDisposable>(o, oo => oo.Dispose());
+            }
+        }
+    }
+}
