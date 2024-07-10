bc4dd6c71e5ead163d43c4f6f20b6d6d192b17e6
ryo1988 <6487424+ryo1988@users.noreply.github.com>
2024-07-10 14:58:42 +0900

    Rml.SharpDx.Vector2Helper.FindClosestPointを追加

--- Rml.SharpDx/Vector2Helper.cs
+++ Rml.SharpDx/Vector2Helper.cs
@@ -32,5 +32,44 @@ namespace Rml.SharpDx
         {
             return NearEqual(left, right, new Vector2(epsilon));
         }
+        
+        public static (Vector2 closestOnLine, float distance) DistanceToLineSegment(Vector2 a, Vector2 b, Vector2 p)
+        {
+            var ap = p - a;
+            var ab = b - a;
+        
+            var ab2 = ab.X * ab.X + ab.Y * ab.Y;
+            var apAb = ap.X * ab.X + ap.Y * ab.Y;
+            var t = apAb / ab2;
+
+            t = t switch
+            {
+                < 0.0f => 0.0f,
+                > 1.0f => 1.0f,
+                _ => t
+            };
+
+            var closest = a + ab * t;
+            return (closest, Vector2.Distance(closest, p));
+        }
+        
+        public static (Vector2 closestPoint, Vector2 closestPointOnLine) FindClosestPoint(Vector2 linePoint1, Vector2 linePoint2, Vector2[] points)
+        {
+            var closestPoint = points[0];
+            var (minClosestOnLine, minDistance) = DistanceToLineSegment(linePoint1, linePoint2, points[0]);
+        
+            foreach (var point in points)
+            {
+                var (closestOnLine, distance) = DistanceToLineSegment(linePoint1, linePoint2, point);
+                if (distance < minDistance)
+                {
+                    minClosestOnLine = closestOnLine;
+                    minDistance = distance;
+                    closestPoint = point;
+                }
+            }
+        
+            return (closestPoint, minClosestOnLine);
+        }
     }
 }
