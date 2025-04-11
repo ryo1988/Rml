3ca9296516618fcb1732eb63e45be62ac190c9e9
ryo1988 <6487424+ryo1988@users.noreply.github.com>
2025-04-11 13:25:51 +0900

    ZLinq用Pairwise追加

---
+++ Rml/ValueEnumerableExtensions.cs
@@ -0,0 +1,17 @@
+﻿using ZLinq;
+using ZLinq.Linq;
+
+namespace Rml;
+
+public static class ValueEnumerableExtensions
+{
+    public static ValueEnumerable<Zip<TEnumerator, Skip<TEnumerator, TSource>, TSource, TSource, (TSource previous, TSource current)>, (TSource previous, TSource current)> Pairwise<TEnumerator, TSource>(this ValueEnumerable<TEnumerator, TSource> source)
+        where TEnumerator : struct, IValueEnumerator<TSource>
+#if NET9_0_OR_GREATER
+        , allows ref struct
+#endif 
+    {
+        return source
+            .Zip(source.Skip(1), (o, i) => (o, i));
+    }
+}
