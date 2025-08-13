5666b1086c2f2823b8bde5922019505c7c057088
ryo1988 <6487424+ryo1988@users.noreply.github.com>
2025-08-13 14:10:01 +0900

    R3Extensions追加

---
+++ Rml/R3Extensions.cs
@@ -0,0 +1,14 @@
+﻿using R3;
+
+namespace Rml;
+
+public static class R3Extensions
+{
+    public static BindableReactiveProperty<T?> ToBindableReactivePropertyAsSynchronized<T>(
+        this ReactiveProperty<T?> source, ref DisposableBuilder db)
+    {
+        var result = source.ToBindableReactiveProperty();
+        result.Subscribe(source, (o, state) => state.Value = o).AddTo(ref db);
+        return result;
+    }
+}
